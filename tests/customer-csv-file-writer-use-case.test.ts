5636627cabef5b1bcc84d5d28b81251c81acb02b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-25 06:19:18 +0000

    25 May 2023, 11:49

---
+++ tests/customer-csv-file-writer-use-case.test.ts
@@ -0,0 +1,23 @@
+import Customer from "../src/Customer"
+import { FileWriter } from "../src/FileWriter"
+import CustomerCsvFileWriter from "../src/customer-csv-file-writer-use-case"
+
+describe('Customer CSV File writer',()=>{
+
+    
+   test('does exist',()=>{
+
+          expect(CustomerCsvFileWriter).toBeInstanceOf(Function)
+   })
+
+   test("?",()=>{
+
+       const customer:Customer=new Customer("Chiranjeev","007");
+
+       const mockFileWriter:FileWriter={
+           writeLine:jest.fn()
+       }
+   })
+
+
+})
