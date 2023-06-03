7d772d0cb92778b8d5c9cfc4ed784c901d9f0af0
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-03 09:07:32 +0000

    3 Jun 2023, 14:37

---
+++ src/CustomerTestHelper.ts
@@ -0,0 +1,17 @@
+import Customer from "./Customer"
+
+
+let customers:Customer[]=[
+                            new Customer("Chiranjeev","007"), 
+                            new Customer("Chinu","001"),
+                            new Customer("Jeevan","007"), 
+                            new Customer("Zero","001"),
+                            new Customer("Veeru","002"), 
+                            new Customer("Heeru","000"),
+                          ]
+
+
+
+export {
+  customers
+}
