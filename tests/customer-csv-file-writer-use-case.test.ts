3d0aaeaa890f37b0c80c15aa9ece6ddcbdecc71b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-03 09:38:49 +0000

    3 Jun 2023, 15:08

--- tests/customer-csv-file-writer-use-case.test.ts
+++ tests/customer-csv-file-writer-use-case.test.ts
@@ -1,3 +1,4 @@
+import Customer from "../src/Customer";
 import CustomerTestHelper from "../src/CustomerTestHelper";
 import { FileWriter } from '../src/FileWriter';
 import CustomerCsvFileWriter from "../src/customer-csv-file-writer-use-case";
@@ -74,4 +75,9 @@ describe('Customer CSV File writer',()=>{
    // this is done to isolate the construction details 
    function createCsvFileWriter(fileWriter:FileWriter):CustomerCsvFileWriter{
         return new CustomerCsvFileWriter(fileWriter)
+   }
+
+   // to remove the constructor details 
+   function createCustomer(name:string,contactNumber:string):Customer{
+     return  new Customer(name,contactNumber)
    }
