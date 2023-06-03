071baaf4f13e05d349e767dcef00c02f488d1d27
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-03 16:35:09 +0000

    3 Jun 2023, 22:05

--- src/CustomerTestHelper.ts
+++ src/CustomerTestHelper.ts
@@ -1,6 +1,33 @@
 import Customer from "./Customer";
 
-  let customers: Customer[] = [
+
+let customers:Customer[]=[
+                            new Customer("Chiranjeev","007"), 
+                            new Customer("Chinu","001"),
+                            new Customer("Jeevan","007"), 
+                            new Customer("Zero","001"),
+                            new Customer("Veeru","002"), 
+                            new Customer("Heeru","000"),
+                          ]
+
+let customerSignatures:string[]=customers.map(customer => customer.toString())
+
+
+let customers11:Customer[]=[
+                            new Customer("Chiranjeev","001"), 
+                            new Customer("Chinu","002"),
+                            new Customer("Jeevan","003"), 
+                            new Customer("Zero","004"),
+                            new Customer("Veeru","005"), 
+                            new Customer("Heeru","006"),
+                            new Customer("Boost","007"), 
+                            new Customer("Complan","008"),
+                            new Customer("Malto","009"), 
+                            new Customer("Horlics","010"),
+                            new Customer("Milo","010"),
+  ]
+
+  let customers20: Customer[] = [
   new Customer("Chiranjeev", "001"),
   new Customer("Chinu", "002"),
   new Customer("Jeevan", "003"),
@@ -31,4 +58,7 @@ import Customer from "./Customer";
 
 export default {
   customers,
+  customers11,
+  customerSignatures,
+  customers20,
 }
