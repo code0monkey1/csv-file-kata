6571571b2aaeaf623350967141c6acaeedc53372
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-03 15:55:25 +0000

    3 Jun 2023, 21:25

--- tests/customer-csv-file-writer-use-case.test.ts
+++ tests/customer-csv-file-writer-use-case.test.ts
@@ -118,7 +118,7 @@ describe('Customer CSV File writer',()=>{
             assertCustomersWereWrittenToFile(mockFileWriter,fileName,CustomerTestHelper.customers11.slice(0,10))
             assertCustomersWereWrittenToFile(mockFileWriter,'customers1.csv',CustomerTestHelper.customers11.slice(10,))
 
-            // expect(mockFileWriter.writeLine).toHaveBeenCalledTimes(CustomerTestHelper.customers11.length)
+            expect(mockFileWriter.writeLine).toHaveBeenCalledTimes(CustomerTestHelper.customers11.length)
 
       })
 
