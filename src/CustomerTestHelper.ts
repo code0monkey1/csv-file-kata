79f26dcc66127d57acee99eaaaecea1e1b2f697d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-03 16:21:57 +0000

    3 Jun 2023, 21:51

--- src/CustomerTestHelper.ts
+++ src/CustomerTestHelper.ts
@@ -1,4 +1,4 @@
-import Customer from "./Customer"
+import Customer from "./Customer";
 
 
 let customers:Customer[]=[
@@ -27,8 +27,38 @@ let customers11:Customer[]=[
                             new Customer("Milo","010"),
   ]
 
+  let customers20: Customer[] = [
+  new Customer("Chiranjeev", "001"),
+  new Customer("Chinu", "002"),
+  new Customer("Jeevan", "003"),
+  new Customer("Zero", "004"),
+  new Customer("Veeru", "005"),
+  new Customer("Heeru", "006"),
+  new Customer("Boost", "007"),
+  new Customer("Complan", "008"),
+  new Customer("Malto", "009"),
+  new Customer("Horlics", "010"),
+  new Customer("Milo", "011"),
+  new Customer("Rajkumar", "012"),
+  new Customer("Rajesh", "013"),
+  new Customer("Ramesh", "014"),
+  new Customer("Ravi", "015"),
+  new Customer("Sachin", "016"),
+  new Customer("Sagar", "017"),
+  new Customer("Sanket", "018"),
+  new Customer("Shantilal", "019"),
+  new Customer("Shivam", "020"),
+  new Customer("Shubham", "021"),
+  new Customer("Siddhant", "022"),
+  new Customer("Srikanth", "023"),
+  new Customer("Sriram", "024"),
+  new Customer("Sushmita", "025"),
+  new Customer("Sushil", "026")
+];
+
 export default {
   customers,
   customers11,
-  customerSignatures
+  customerSignatures,
+  customers20,
 }
