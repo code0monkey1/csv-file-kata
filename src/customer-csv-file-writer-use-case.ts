9cd590abf20b7f4244726567d5fcae0a2b27cfef
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-03 13:50:55 +0000

    3 Jun 2023, 19:20

--- src/customer-csv-file-writer-use-case.ts
+++ src/customer-csv-file-writer-use-case.ts
@@ -12,7 +12,8 @@ class CustomerCsvFileWriter{
               if(customers===null){
                  throw new Error("argument is null : `customers`");
               }
-
+              let fileNumber=1
+              
               customers
                      .forEach(customer => 
                                    this
