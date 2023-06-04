9db75ddbb385d68fa1b552ce429861f64b8dab35
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 11:13:23 +0000

    4 Jun 2023, 16:43

--- tests/filter-unique-entries-use-case.test.ts
+++ tests/filter-unique-entries-use-case.test.ts
@@ -1,17 +1,18 @@
 describe("Filter Unique Entries",()=>{
    
    test("given a files array , only write entries that are not duplicates",()=>{
-        const entries =[["a","b","a"]]
+        const entries =[["a","b","a"],["a","b","a"],["a","b","a"]]
         const result=[]
         const mySet = new Set()
         
         for( let entry of entries){
            
            for (let individualEntry of entry){
-            if (!mySet.has(individualEntry)){  
-                result.push(individualEntry)
-                mySet.add(individualEntry)
-              }
+
+              if (!mySet.has(individualEntry)){  
+                  result.push(individualEntry)
+                  mySet.add(individualEntry)
+                }
             }
         }
          expect(result.length).toBe(2)
