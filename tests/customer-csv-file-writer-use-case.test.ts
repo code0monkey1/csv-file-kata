14ecb58e67655e79241b15969ecbd30ef444b97a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 05:08:41 +0000

    4 Jun 2023, 10:38

--- tests/customer-csv-file-writer-use-case.test.ts
+++ tests/customer-csv-file-writer-use-case.test.ts
@@ -69,9 +69,9 @@ describe('Customer CSV File writer',()=>{
         {
             customers:createCustomers(11)
         }, 
-        // {
-        //     customers:createCustomers(20)
-        // }
+        {
+            customers:createCustomers(20)
+        }
         
     ])("for customer: $customer._name $customer._contactNumber",({customers})=>{
             
@@ -85,7 +85,7 @@ describe('Customer CSV File writer',()=>{
             const fileName = 'customers.csv';
 
             //Act
-                sut.writeCustomers(fileName,customers)
+            sut.writeCustomers(fileName,customers)
                 
             //Assert
              assertCustomersWereWrittenToFile(mockFileWriter,fileName,customers)
