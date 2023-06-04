db332b6f6071ddf7cd112c3e9c97a316b815c38c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 11:03:19 +0000

    4 Jun 2023, 16:33

--- tests/filter-unique-entries-use-case.test.ts
+++ tests/filter-unique-entries-use-case.test.ts
@@ -1,7 +1,11 @@
 describe("Filter Unique Entries",()=>{
    
    test("given a files array , only write entries that are not duplicates",()=>{
+        const entries =["a","b","a"]
         
+        const mySet = new Set(entries)
+     
+         expect(mySet).toHaveLength(2)
 
   })
   }
