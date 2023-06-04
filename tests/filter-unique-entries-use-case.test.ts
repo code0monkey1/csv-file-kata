1eaa8be7a7b6fd1c6075578d8d12221c0076c0f1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 12:06:51 +0000

    4 Jun 2023, 17:36

--- tests/filter-unique-entries-use-case.test.ts
+++ tests/filter-unique-entries-use-case.test.ts
@@ -37,6 +37,38 @@ describe("Filter Unique Entries",()=>{
                 expect(mockFileWriter.writeLine).toBeCalledTimes(2)
      
   })
+
+   test("given a files array , only write entries that are not duplicates",()=>{
+                 
+               // Arrange
+              
+                const mockFileWriter:FileWriter=FileWriterHelper.createFileWriter()
+        
+                const customerCsvFileWriter = FileWriterHelper. createCsvFileWriter(mockFileWriter)
+
+                const batchedCustomerCsvFileWriter = new BatchedCustomerCsvFileWriter(customerCsvFileWriter,10)
+                 
+                const sut = new FilterUniqueEntries(batchedCustomerCsvFileWriter)
+                
+                const fileName = 'customers.csv';
+
+                const customers = [
+                    
+                      CustomerHelper.createCustomer("a","1"),
+                      CustomerHelper.createCustomer('a',"1"),
+                      CustomerHelper.createCustomer("b","2"),
+                      CustomerHelper.createCustomer("a","1"),
+                      CustomerHelper.createCustomer('a',"1"),
+                      CustomerHelper.createCustomer("b","2")
+                ]
+
+              // Apply
+
+                sut.writeCustomers(fileName,customers)
+
+                expect(mockFileWriter.writeLine).toBeCalledTimes(2)
+     
+  })
   }
 
 )
