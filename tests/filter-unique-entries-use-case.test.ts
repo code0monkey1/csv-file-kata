856ba85cfa34c08bd06b86ecc7ecaffb491b594f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 11:49:08 +0000

    4 Jun 2023, 17:19

--- tests/filter-unique-entries-use-case.test.ts
+++ tests/filter-unique-entries-use-case.test.ts
@@ -1,11 +1,14 @@
 import BatchedCustomerCsvFileWriter from "../src/BatchedCustomerCsvFileWriter"
 import { FileWriter } from "../src/FileWriter"
+
 import FilterUniqueEntries from "../src/filter-unique-entries-use-case"
+import CustomerHelper from "./CustomerHelper"
 import FileWriterHelper from './FileWriterHelper'
 describe("Filter Unique Entries",()=>{
    
    test("given a files array , only write entries that are not duplicates",()=>{
-          
+                 
+               // Arrange
               
                 const mockFileWriter:FileWriter=FileWriterHelper.createFileWriter()
         
@@ -15,7 +18,13 @@ describe("Filter Unique Entries",()=>{
 
                 const fileName = 'customers.csv';
 
+                const customers = CustomerHelper.createCustomers(20)
 
+              // Apply
+
+                sut.writeCustomers(fileName,customers)
+
+                expect(mockFileWriter.writeLine).toBeCalledTimes(20)
      
   })
   }
