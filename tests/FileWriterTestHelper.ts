5b262e3deedcd03d98a9ecefc7b51b5c800e663e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 13:59:07 +0000

    4 Jun 2023, 19:29

--- tests/FileWriterTestHelper.ts
+++ tests/FileWriterTestHelper.ts
@@ -2,18 +2,18 @@ import Customer from "../src/Customer"
 import { FileWriter } from "../src/FileWriter"
 import CustomerCsvFileWriter from "../src/customer-csv-file-writer-use-case"
 
-  function createFileWriter():FileWriter{
+  export function createFileWriter():FileWriter{
        return {
             writeLine:jest.fn()
         }
    }  
    // this is done to isolate the construction details 
-   function createCsvFileWriter(fileWriter:FileWriter):CustomerCsvFileWriter{
+  export  function createCsvFileWriter(fileWriter:FileWriter):CustomerCsvFileWriter{
         return new CustomerCsvFileWriter(fileWriter)
    }
 
 
- function formatAsCsvRow(customer:Customer):string{
+ export function formatAsCsvRow(customer:Customer):string{
            return `${customer.name},${customer.contactNumber}`
        }
 
