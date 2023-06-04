733d27d0a2b4c2be74b5cc821d5d55129e11e33f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 13:35:09 +0000

    4 Jun 2023, 19:05

--- src/filter-unique-entries-use-case.ts
+++ src/filter-unique-entries-use-case.ts
@@ -13,18 +13,10 @@ export default class FilterUniqueEntries implements ICustomerFileWriter{
 
               // getting only the element with the first index match , ignoring further index matches
               const uniqueCustomers:Customer[] =customers
-                                                .filter((cust,index,array)=> array
-                                                  .findIndex(customer => cust.name===customer.name )===index)
+                                                .filter((cust,index,array)=> 
+                                                    array
+                                                    .findIndex(customer => cust.name===customer.name )===index)
 
-
-              // customers.forEach( customer =>{ 
-              
-              //      if(!customerSet.has(customer.name)){
-              //           uniqueCustomers.push(customer)
-              //           customerSet.add(customer.name)
-              //      }
-              // })
-            
               this.customerFileWriter.writeCustomers(fileName,uniqueCustomers)
                 
            }
