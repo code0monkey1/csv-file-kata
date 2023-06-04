813ecc5a7760f203395996d269b5eb0a6717baef
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 12:01:12 +0000

    4 Jun 2023, 17:31

--- src/filter-unique-entries-use-case.ts
+++ src/filter-unique-entries-use-case.ts
@@ -9,15 +9,21 @@ class FilterUniqueEntries{
             public writeCustomers(fileName:string,customers:Customer[]){
                 
               const customerSet = new Set<string>()
+               
+              const uniqueCustomers:Customer[] =[]
 
               customers.forEach( customer =>{
                        
                    const csvEntry =  FileWriterHelper.formatAsCsvRow(customer)
+
+                   if(!customerSet.has(csvEntry)){
+                        uniqueCustomers.push(customer)
+                        customerSet.add(csvEntry)
+                   }
               })
             
-              this.batchedCustomerCsvWriter.writeCustomers(fileName,customers)
+              this.batchedCustomerCsvWriter.writeCustomers(fileName,uniqueCustomers)
                 
-
            }
 
        }
