32dc413040743a5a3da79fde2a43a60612f0060b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-04 04:52:46 +0000

    4 Jun 2023, 10:22

---
+++ src/BatchedCustomerCsvFileWriter.ts
@@ -0,0 +1,37 @@
+import Customer from './Customer';
+import CustomerCsvFileWriter from './customer-csv-file-writer-use-case';
+class BatchedCustomerCsvFileWriter{
+
+       constructor(private readonly customerCsvFileWriter:CustomerCsvFileWriter){}
+    
+       public writeCustomersBatched(fileName:string,customers:Customer[]){
+              
+              const extIndex=fileName.lastIndexOf('.')
+              
+              if(extIndex===-1)
+                     throw new Error("File Extension Missing")
+              
+              const baseFileName = fileName.substring(0, extIndex)
+
+              const ext = fileName.substring(extIndex)
+            
+              const BATCH_SIZE = 10
+
+              let fileCount = 0
+
+              for (let i = 0 ;i<customers.length;i+=BATCH_SIZE){
+
+                     const nextFileName = `${baseFileName}${fileCount || ''}${ext}`;
+                    
+                     const nextCustomers = customers.slice(i, i + BATCH_SIZE);
+                     
+                     this.customerCsvFileWriter.writeCustomers(nextFileName,nextCustomers)
+
+                     fileCount++
+              }
+
+       }
+
+}
+
+export default BatchedCustomerCsvFileWriter;
